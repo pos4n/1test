714032d27f62a2b11f7081d8a24b5db0bb747650
pos4n <posan.gc@g.austincc.edu>
2024-07-14 14:58:03 -0500

    Update main.h

---
+++ main.h
@@ -0,0 +1,33 @@
+ /**************************
+ * Name: Posan Gc
+ * Assignment 7: Hash Tables
+ * Purpose of the file: This project implements a hash table using separate chaining with linked lists.
+ * It defines the HashTable class and its interface, including methods for adding, deleting, retrieving,
+ * and printing entries. The project demonstrates the functionality and testing of the hash table implementation
+ * through a main file that generates test data, creates a hash table, and performs various operations.
+ ***************************/
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+//Standard
+#include <iostream>     /* cout, endl */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <string>       /* string */
+
+//Local
+#include "hashtable.h"  /* Hashtable header file*/
+
+
+#define BUFFERSIZE 8
+#define MAXID 100
+#define BASE 5
+#define OFFSET 20
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#endif /* MAIN_H */
